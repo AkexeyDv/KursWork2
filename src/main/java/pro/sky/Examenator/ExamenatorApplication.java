052a42f2884a6b03128d5532f61f7606009e2282
AlexDv <DvoinikovAV@yandex.ru>
2024-04-12 10:55:56 +0300

    Начнемс...

---
+++ src/main/java/pro/sky/Examenator/ExamenatorApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.Examenator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenatorApplication.class, args);
+	}
+
+}
